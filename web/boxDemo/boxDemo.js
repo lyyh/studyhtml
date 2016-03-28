981a60f147d0873782a06fc9b4deb941fdaf67a3
liuyanhao <374659635@qq.com>
2016-03-28 15:59:46 +0800

    浮动和定位

--- web/boxDemo/boxDemo.js
+++ web/boxDemo/boxDemo.js
@@ -1,33 +1,23 @@
 /**
  * Created by Administrator on 2016/3/27 0027.
  */
-function preUnfold(){
-    //获得display属性
-    var sit = $(".ul-character .li-expression").css("display");
+/**
+ * 控制图片展开和收缩
+ * @param ltVal li-expression的前几个元素
+ * @param eqVal .title a所有元素的索引
+ */
+function foldAndUnfold(val,eqVal){
+    var sit = $(".li-expression:"+val).css("display");
     if(sit=="none"){
-        $(".ul-character .li-expression").css('display','block');
-        $(".lan").css("margin-top","20px");
-        $(".per a").html("收起");
+        //$(".li-expression:lt(ltVal)").css('display','block');
+        $(".li-expression:"+val).css('display','block');
+        $(".title a:eq(eqVal)").html("收起");
     }else if(sit=="block"){
-        $(".ul-character .li-expression").css('display','none');
-        $(".per a").html("展开");
+        //$(".li-expression:lt(ltVal)").css('display','none');
+        $(".li-expression:"+val).css('display','none');
+        $(".title a:eq(eqVal)").html("展开");
     }
+    //}
     //$("#id")[0].style.display="none";
     //$("#fold")[0].innerHTML="收起";
 }
-
-function lanUnfold(){
-    //获得display属性
-    var sit = $(".ul-framework .li-expression").css("display");
-    if(sit=="none"){
-        $(".ul-framework .li-expression").css('display','block');
-        $
-        $(".lan a").html("收起");
-    }else if(sit=="block"){
-        $(".ul-framework .li-expression").css('display','none');
-        $(".lan a").html("展开");
-    }
-    //$("#id")[0].style.display="none";
-    //$("#fold")[0].innerHTML="收起";
-}
-
